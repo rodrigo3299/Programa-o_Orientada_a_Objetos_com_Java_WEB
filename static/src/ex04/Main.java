a16993151c98e0c34424b0ee3b382dfdb7d72143
rodrigo3299 <99183265+rodrigo3299@users.noreply.github.com>
2026-03-04 22:42:00 -0300

    Update Main.java

--- static/src/ex04/Main.java
+++ static/src/ex04/Main.java
@@ -1,4 +1,12 @@
 package ex04;
 
 public class Main {
+    public static void main(String[] args) {
+        Carro c1 = new Carro("a1", "fieste");
+        Carro c2 = new Carro("a2", "q5");
+        Carro c3 = new Carro("a3", "bmw x7");
+
+        System.out.println(Carro.getTotal());
+
+    }
 }
