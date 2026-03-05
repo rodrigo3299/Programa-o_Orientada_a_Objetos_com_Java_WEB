de41d92801d78976ac3ff83d5cefdf20e9bbb99f
rodrigo3299 <99183265+rodrigo3299@users.noreply.github.com>
2026-03-04 22:14:12 -0300

    Update Main.java
    att bilhete

--- static/src/ex03_static/Main.java
+++ static/src/ex03_static/Main.java
@@ -1,4 +1,23 @@
-package ex03_static;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String usuario, tipoTarifa;
+
+        System.out.print("Usúario do bilhete único --> ");
+        usuario = sc.next();
+        System.out.print("Digite o tipo de tarifa: Professor, Estudante ou Normal --> ");
+        tipoTarifa = sc.next();
+
+        BilheteUnico bilhete = new BilheteUnico(usuario, tipoTarifa);
+
+
+        System.out.println("Saldo atual R$ " + bilhete.saldo);
+        bilhete.passarNaCatraca();
+        System.out.println("Saldo atual R$ " +bilhete.saldo);
+        bilhete.Carregar(10);
+        bilhete.passarNaCatraca();
+        System.out.println("Saldo atual R$ " +bilhete.saldo);
+    }
 }
