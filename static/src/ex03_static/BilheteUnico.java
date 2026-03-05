acc70314e7e214ee2ff92a5b56e7aa0a6aae738c
rodrigo3299 <99183265+rodrigo3299@users.noreply.github.com>
2026-03-04 22:14:41 -0300

    Update BilheteUnico.java
    att bilhete

--- static/src/ex03_static/BilheteUnico.java
+++ static/src/ex03_static/BilheteUnico.java
@@ -1,18 +1,39 @@
-package ex03_static;
-
 import java.util.Random;
 
-public class BilheteUnico {
-    int numero;
-    String usuario;
-    double saldo;
-    final static double tarifaBase = 5.40;
-    String tipoTarifa;
+    public class BilheteUnico {
+        int numero;
+        String usuario;
+        double saldo;
+        final static double tarifaBase = 5.40;
+        String tipoTarifa;
 
-    public BilheteUnico( String usuario, String tipoTarifa){
-        Random rd = new Random();
-        this.numero = rd.nextInt(1000,9999);
-        this.usuario = usuario;
-        this.tipoTarifa = tipoTarifa;
+        public BilheteUnico(String usuario, String tipoTarifa) {
+            Random rd = new Random();
+            this.numero = rd.nextInt(1000, 9999);
+            this.usuario = usuario;
+            this.tipoTarifa = tipoTarifa;
+            this.saldo = 0.0;
+        }
+
+        public BilheteUnico() {
+
+        }
+
+        public void Carregar(double valor) {
+            this.saldo += valor;
+        }
+
+        public void passarNaCatraca() {
+            double valor = tarifaBase;
+            if (tipoTarifa.equalsIgnoreCase("professor") ||
+                    tipoTarifa.equals("estudante")) {
+                valor = tarifaBase /2;
+            }
+            if (saldo <  valor){
+                return;
+            }
+
+            saldo -= valor;
+        }
     }
-}
+
